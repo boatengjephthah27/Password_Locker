d14ca27d7506f2a25fa6388e333c1b6cff65f9fe
Boateng Jephthah Agyenim <Boatengjephthah@gmail.com>
2022-08-06 06:39:07 +0000

    numbers , letters symbols generated

--- PasswordLocker.py
+++ PasswordLocker.py
@@ -1,12 +1,29 @@
 from tkinter import *
 from tkinter.font import BOLD
+import string
+import random as ran
 
 # **************************************** PASSWORD GENRATOR / FUNCTIONS ********************************************
 
+def generate():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    
+    symbols = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~']
+    
+    password = ""
+    passlist = []
+    
+    letters_pass = ran.choice()
+    
+        
+    print(letters_pass)
+        
+    
 
 
-
-
+generate()
 
 
 # **************************************** SAVE DETAILS ********************************************
